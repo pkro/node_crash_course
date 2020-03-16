b9e7beb6839e18931e25dcca9a890f880e34f265
pkro <pkrombach@web.de>
2020-03-16 09:33:43 +0100

    commetn for import

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const Person = require('./person');
+/* Note: 
+import Person from './person' doesn't work in nodejs (yet), Syntax is only supported by babbel
+-> pkr: it might be supported now with additional 
+*/
 
 const me = new Person('Pkro', 40);
 me.greet();
+
+export default me;
