9f842f27c8d5c0d25646243b31ae55583f8e5566
pkro <pkrombach@web.de>
2020-03-16 15:34:52 +0100

    html serving via route

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const http = require("http");
 const path = require("path");
 const fs = require("fs");
 
-const server = http
-  .createServer((req, res) => {
-    if(req.url === '/') {
-        res.end('<h1>Home</h1>');
-    }
-  });
-  
-  const PORT = process.env.PORT || 5000;
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    fs.readFile(path.join(__dirname, "public", "index.html"), (err, data) => {
+      if (err) throw err;
+      res.writeHead(200, { "Content-type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  }
+});
 
-  server.listen(PORT, () => console.log(`server running on ${PORT}`));
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`server running on ${PORT}`));
