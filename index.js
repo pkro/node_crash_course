74b556fc8c89b1318dddd27ed15c10c735f624f3
pkro <pkrombach@web.de>
2020-03-16 15:37:50 +0100

    refactored file serving into function

--- index.js
+++ index.js
@@ -4,15 +4,21 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
-    fs.readFile(path.join(__dirname, "public", "index.html"), (err, data) => {
-      if (err) throw err;
-      res.writeHead(200, { "Content-type": "text/html" });
-      res.write(data);
-      res.end();
-    });
+    serveFile(res, 'index.html');
+  }
+  else if (req.url === "/about") {
+    serveFile(res, 'about.html');
   }
 });
 
+const serveFile = (res, file) => {
+    fs.readFile(path.join(__dirname, "public", file), (err, data) => {
+        if (err) throw err;
+        res.writeHead(200, { "Content-type": "text/html" });
+        res.write(data);
+        res.end();
+      });
+}
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => console.log(`server running on ${PORT}`));
