6e1c77739394926ca48e58845e3a4f82ff3d6b58
pkro <pkrombach@web.de>
2020-03-16 09:27:34 +0100

    added explanation for module wrapper function

--- person.js
+++ person.js
@@ -1,3 +1,11 @@
+/* this gets automatically wrapped in a module wrapper function automatically:
+Note the parameters we have access to (in ths example, __dirname)
+(function(exports, require, module, __filename, __dirname)) {
+
+})
+
+*/
+
 class Person  {
     constructor(name, age) {
         this.name = name;
@@ -6,7 +14,7 @@ class Person  {
 
     greet() {
         const {name, age} = this;
-        console.log(`My name is ${name} and i am ${age}`);
+        console.log(`My name is ${name} and i am ${age} and my class lives in ${__dirname}`);
     }
 }
 
